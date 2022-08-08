1c8308df0d47f5003ec3ba41f9d2657deaaacf76
heokyeongju <beewt@naver.com>
2022-08-08 14:12:16 +0900

    sevlet req, resp 사용 더하기

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -3,6 +3,9 @@ package com.ll.exam.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -76,5 +79,13 @@ public class MainController {
         return IntStream.range(1,limit)
                 .mapToObj(i -> "%d * %d = %d".formatted(finalDan,i,finalDan *i ))
                 .collect(Collectors.joining("<br/>"));
+    }
+    @GetMapping("/plus2")
+    @ResponseBody
+    public void showPlus2(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        int a = Integer.parseInt(req.getParameter("a"));
+        int b= Integer.parseInt(req.getParameter("b"));
+
+        resp.getWriter().append(a + b + "");
     }
 }
