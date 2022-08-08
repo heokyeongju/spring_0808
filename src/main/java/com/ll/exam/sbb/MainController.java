e03a1d347b939ddc293a411efe522971712d3a85
heokyeongju <beewt@naver.com>
2022-08-08 13:35:57 +0900

    get, post 요청 관리

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,16 +1,62 @@
 package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
+    private int i=-1;
     @RequestMapping("/sbb")
     //  @ResponseBody : 함수의 return값을 화면에 보여줌
     @ResponseBody
     public String index(){
-        System.out.println("hi there"); // 서버에서 출력
-        return "hello"; // 브라우저에서 출력
+        System.out.println("hi there"); // 서버에서 출력 (콘솔)
+        return "okay"; // 브라우저에서 출력
+    }
+
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showPage1() {
+        return """
+                <form method="POST" action="/page2">
+                    <input type="number" name="age" placeholder="나이" />
+                    <input type="submit" value="page2로 POST 방식으로 이동" />
+                </form>
+                """;
+    }
+
+    @PostMapping("/page2")
+    @ResponseBody
+    public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
+                """.formatted(age);
+    }
+
+    @GetMapping("/page2")
+    @ResponseBody
+    public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>안녕하세요, GET 방식으로 오셨군요.</h1>
+                """.formatted(age);
+    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int showPlus(int a, int b) {
+        return a+b;
+    }
+    @GetMapping("/minus")
+    @ResponseBody
+    public int showMinus(int a, int b) {
+        return a-b;
+    }
+    @GetMapping("/increase")
+    @ResponseBody
+    public int showIncrease() {
+
+        return ++i;
     }
 }
