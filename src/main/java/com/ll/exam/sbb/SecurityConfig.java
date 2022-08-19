a03fa21f812cd0d49e8e16579a215dc952f8efbb
heokyeongju <beewt@naver.com>
2022-08-19 11:10:38 +0900

    signup, login

--- src/main/java/com/ll/exam/sbb/SecurityConfig.java
+++ src/main/java/com/ll/exam/sbb/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.ll.exam.sbb;
 
+import com.ll.exam.sbb.user.UserSecurityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,26 +16,36 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 // 시큐리티 설정
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserSecurityService userSecurityService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/**").permitAll()
-                .and()
+                .and() // 문맥의 끝
                 .csrf().ignoringAntMatchers("/h2-console/**")
-                .and()// 문맥의 끝
+                .and() // 문맥의 끝
                 .headers().addHeaderWriter(new XFrameOptionsHeaderWriter(
-                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
-                // 모든 경로
-                //.antMatchers("/**")
-                // 허용 한다.
-                //.permitAll();
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and()
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/");
+
         return http.build();
     }
+
     // 스프링 시스템에 객체를 등록한다.
     // @Configuration 라는 어노테이션을 가진 클래스에서만 사용가능하다.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
